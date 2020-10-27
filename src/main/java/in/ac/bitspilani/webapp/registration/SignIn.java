7c81bf4d93ffa8a4aa9ede9efc45501ac29301c4
sahithi <sahithi02.ra@gmail.com>
2020-10-27 21:34:57 +0530

    reg commit

--- src/main/java/in/ac/bitspilani/webapp/registration/SignIn.java
+++ src/main/java/in/ac/bitspilani/webapp/registration/SignIn.java
@@ -1,4 +1,44 @@
 package in.ac.bitspilani.webapp.registration;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class SignIn {
+    private String username;
+    private String password;
+    private boolean status;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setStatus() {
+        if(username.equals("Sahithi")&&password.equals("sahithi")) {
+            status = true;
+        }
+        else
+        {
+            status=true;
+        }
+    }
+    @RequestMapping
+    public String next()
+    {
+        if(status)
+        {
+            return "/dashboard";
+        }
+        return "/error";
+    }
 }
