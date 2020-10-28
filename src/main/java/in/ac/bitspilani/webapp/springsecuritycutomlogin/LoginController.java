82f1ac61546112bfa5bbf95a47ef65680337662e
Kanti Sonnathi <sonnathikanti@gmail.com>
2020-10-28 13:01:53 +0530

    removed security application

--- src/main/java/in/ac/bitspilani/webapp/springsecuritycutomlogin/LoginController.java
+++ src/main/java/in/ac/bitspilani/webapp/springsecuritycutomlogin/LoginController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login()
-    {
-        return "Registration";
+    public String login() {
+        return "registration/Registration";
     }
 }
