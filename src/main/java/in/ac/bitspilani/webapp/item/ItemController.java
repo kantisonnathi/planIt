864b29e267869a8543b9c6c2c53dc4b238196bf0
unknown <shreya.guda@gmail.com>
2020-11-25 16:09:25 +0530

    halfway through update

--- src/main/java/in/ac/bitspilani/webapp/item/ItemController.java
+++ src/main/java/in/ac/bitspilani/webapp/item/ItemController.java
@@ -67,7 +67,6 @@ public class ItemController {
 
     @GetMapping("/itemDetails")
     public String ItemDetails(@PathVariable("categoryId") int categoryId,User user, Map<String, Object> map) {
-        ModelAndView mav = new ModelAndView("dashboard/categoryDetails");
         Category category = categoryRepository.findById(categoryId);
         map.put("selections", category.getItems());
         map.put("user", user);
